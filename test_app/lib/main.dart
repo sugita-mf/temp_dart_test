ce39b9eab0ea476b84777a20fbe166420b668011
sugita-mf <sugita.shinsuke@moneyforward.co.jp>
2026-08-20 14:13:25 +0900

    add My Home Page Preview

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widget_previews.dart';
 
 void main() {
   runApp(const MyApp());
@@ -119,4 +120,10 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+@Preview(name: 'My Home Page Preview')
+// @Preview(name: 'My Home Page Preview', group: 'Auth/Buttons')
+Widget previewMyHomePage() {
+  return const MyApp();
 }
