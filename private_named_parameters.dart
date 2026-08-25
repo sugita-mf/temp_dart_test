c5fa271aa53c3b2922899e631ee07f6468b6549f
sugita-mf <sugita.shinsuke@moneyforward.co.jp>
2026-08-25 13:24:09 +0900

    add _id example

--- private_named_parameters.dart
+++ private_named_parameters.dart
@@ -12,6 +12,8 @@ class MyWidget {
 void main() {
   // Call using public parameter name 'id:' (WITHOUT leading underscore)
   final widget = MyWidget(id: 'widget_123');
+  // ❌ Error: No named parameter with the name '_id'.
+  // final widget = MyWidget(_id: 'widget_123');
 
   // Accessible inside same file or via getter if defined
   print('Widget ID: ${widget._id}');
